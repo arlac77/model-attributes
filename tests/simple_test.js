c1a033b39b10b4fe2ccc0c207558540042c11bae
Markus Felten <Markus.Felten@gmx.de>
2016-06-13 17:30:20 +0200

    test: increase coverage

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -12,16 +12,69 @@ const atts = require('../attributes');
 
 describe('attributes', () => {
   const md = {
-    att1: {}
+    att1: {},
+    att2: {
+      setter(value, attribute) {
+          this.att2x = value;
+          return true;
+        },
+        getter(attribute) {
+          return this.att2x;
+        }
+    },
+    nested: {
+      attributes: {
+        att1: {}
+      }
+    }
   };
 
-  it('set simple', () => {
-    const object = {};
+  describe('set', () => {
+    it('simple', () => {
+      const object = {};
 
-    atts.setAttributes(object, md, {
-      att1: 'value1'
+      atts.setAttributes(object, md, {
+        att1: 'value1'
+      });
+
+      assert.equal(object.att1, 'value1');
     });
 
-    assert.equal(object.att1, 'value1');
+    it('nested simple', () => {
+      const object = {};
+
+      atts.setAttributes(object, md, {
+        nested: { 
+          att1: 'value1'
+        }
+      });
+
+      assert.equal(object.nested.att1, 'value1');
+    });
+
+    it('with setter', () => {
+      const object = {};
+
+      atts.setAttributes(object, md, {
+        att2: 'value2'
+      });
+
+      assert.equal(object.att2x, 'value2');
+    });
+  });
+
+  describe('get', () => {
+    const object = {
+      'att1': 'value1',
+      'att2x': 'value2'
+    };
+
+    it('simple', () => {
+      assert.equal(atts.getAttribute(object, md, 'att1'), 'value1');
+    });
+
+    it('with getter', () => {
+      assert.equal(atts.getAttribute(object, md, 'att2'), 'value2');
+    });
   });
 });
