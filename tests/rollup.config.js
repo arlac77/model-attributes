0951a366a7b5fec0eabf3acdf487813ce8a86f14
Markus Felten <Markus.Felten@gmx.de>
2018-07-16 22:51:44 +0200

    chore(package): add rollup-plugin-commonjs@^9.1.3 chore(package): add rollup-plugin-node-resolve@^3.3.0 chore(package): add rollup-plugin-multi-entry@^2.0.2 chore(package): add rollup-plugin-istanbul@^2.0.1 chore(rollup): import resolve chore(rollup): import commonjs chore(rollup): import multiEntry chore(rollup): import istanbul chore(rollup): add multiEntry to plugins chore(rollup): add istanbul to plugins chore(rollup): add resolve to plugins chore(rollup): add commonjs to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,7 +1,14 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import multiEntry from 'rollup-plugin-multi-entry';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 export default {
   input: 'tests/attributes-test.js',
   external: ['ava'],
-  plugins: [],
+  plugins: [multiEntry(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  }), resolve(), commonjs()],
   output: {
     file: 'build/attributes-test.js',
     format: 'cjs',
